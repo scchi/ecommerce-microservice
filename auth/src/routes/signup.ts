f2b57fbb0bb72aa358c1ceadfba85ac818cfc46c
Sheldon Chi <sheldonchi@gmail.com>
2022-06-23 00:13:16 +0800

    Add JWT on user sign up

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -22,11 +25,28 @@ async (req: Request, res: Response) => {
   }
 
   const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError(); 
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
+  }
 
-  res.send({});
+  const user = User.build({ email, password });
+  await user.save();
+
+  const userJwt = jwt.sign(
+    {
+    id: user.id,
+    email: user.email
+    }, 
+    process.env.JWT_KEY!
+  );
+
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
